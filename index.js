5fb21d13448871bc04bce3d319efeb45ccd59410
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-02-02 17:39:23 -0300

    terminado el envio de datos encryptados por llave publica desde el front hasta el back que lo desencripta

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const bodyParser = require('body-parser');
+const cors = require('cors');
+// configuraciones de la app express
+app.use(bodyParser.json());
+app.use(cors());
 // Generador De LLaves Publicas Y Privadas
 const NodeRSA = require('node-rsa');
 const key = new NodeRSA({ b: 1024 });
+// La libreria del front para encryptar los datos usa el pkcs1 y tenemos que establecer que nosotros en node-rsa usaremos su mismo mecanismo para poder encryptar y descrifrar jsencrypt
+key.setOptions({encryptionScheme: 'pkcs1'});
 // const public_key = key.exportKey('public');
 // const private_key = key.exportKey('private');
 
@@ -14,29 +20,32 @@ const key = new NodeRSA({ b: 1024 });
 const secret = ' hola';
 
 const valor_públic_key = ` -----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDFNtc8kG2PYBz4IgBl3KK8cLvM
-zrRxZ4aTGNUEdOxEGhvJIfC3tzi+zc2PuEtoNPkh3T0uDOrq6KH8wX3Fh0+UBIKh
-h3BlNyQEMLRmqOeJjSIdx408xGUHvtD9x69DUA9YwhxOfdeFqQzXhhjgYPiITlUA
-a07xXumzwUoOESLTDQIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHz8NVuztXCwpdMl80xaXQei+k
+nUduRLXufCuxQSikQfhWRUmW9UgwAc3NNYV8qsM2HmcKSgCsmmUALu+ifsYzBvEj
+Wo1wjahcbbW8s260JVTLgi5qZbLjSV1EIhBoclLnkk1WivUWGaPprvgKuS5aP8Gj
+YuBgLO1eLEJg/w3h3QIDAQAB
 -----END PUBLIC KEY-----`;
 const valor_private_key = ` -----BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQDFNtc8kG2PYBz4IgBl3KK8cLvMzrRxZ4aTGNUEdOxEGhvJIfC3
-tzi+zc2PuEtoNPkh3T0uDOrq6KH8wX3Fh0+UBIKhh3BlNyQEMLRmqOeJjSIdx408
-xGUHvtD9x69DUA9YwhxOfdeFqQzXhhjgYPiITlUAa07xXumzwUoOESLTDQIDAQAB
-AoGAYid7VK9leBiUEoOJwyje7f8F2Vd/w3mNJyUS0FcJSJPeBGsaDbXpTj3L8538
-Ge9Tl6EvU/4l5ajHBq+e03MXDWlorutG1Vg+BQVACiOvkPxKmP6ZHQG42Z4/y/Ua
-rv/fKI39vMeReBUbHP6QEvmdK6kFBpnik22zZ/Bq//O10cECQQD7SMbxfcAtYPI6
-d/VrGAOe9YeSycAmYLBAHrNN1hydkPfjk50YINCn69MaFxyyQwujfIJ4ULq4cUca
-zwxWcPA5AkEAyOpMs8IS4FPJBjkOfqQVrutLHmEKChNCS6elltHR5gJuxIWgQwWO
-GmQ6/JkR6wPd+H4cy1Fdd2eZp5JoaZtRdQJBALtwwCWnPdEbvdRrYv/tu9W5JNYT
-9GcsPIy32dw32MSYPLDfjZFP+RUpTiDqGJfId0gOOmxxG7c1mGqhA3z/ZdECQQCZ
-1rV2aqLTe7QP/L45f+BOeEGjeEAcqNR5Y1uqWOx0RuaTuZGV3/oIATkdUwNiRSrA
-a6gWki2UERKZOcSTIGdFAkA7/CDhpaP3REZtCc2hsq4XOn9PaD2I1BM/L1wD96sX
-I1ahIjSgpabYLaCNbHT6hGG0zcd6W+8g3rgw+1S7e4Ma
------END RSA PRIVATE KEY-----`
+MIICXAIBAAKBgQDHz8NVuztXCwpdMl80xaXQei+knUduRLXufCuxQSikQfhWRUmW
+9UgwAc3NNYV8qsM2HmcKSgCsmmUALu+ifsYzBvEjWo1wjahcbbW8s260JVTLgi5q
+ZbLjSV1EIhBoclLnkk1WivUWGaPprvgKuS5aP8GjYuBgLO1eLEJg/w3h3QIDAQAB
+AoGAd43Ttb9CoTFE8UCLgLrmHNiPGzLUI6FPLjPzvKpdE/M5EP2CK6X1nGvn5Ooq
+Rc6y4PXN93UR9DSpTDPJRHQQSDXQXrQWBF3un/Y8ClBr4fhr8cfT/DXPaoyStTY+
+JPoqc/bifocDxB3lpaueGve1+kbDa8ofm0H2ZAngAkQPcMECQQD6G0L3GKddRKyi
+l/h/2u/QOO0UAUCro/F/agIOrAT8U3o2HHSg3aswqQiaWTc2uQwB9EJJgNy2H0Ti
+YZMGqCzNAkEAzIUaarlj8I3me69E1BWWzRN/ghv62Ou4nowkh0ALEFB7ySkpxQwS
+AFjLM7XZJRULM9XWKDqwyz1h99JgM1aJUQJAXbszyIj7yC+Bh1nu7nOtplp/Nx0V
+EH5bdkXTSY5cO/hI99iIOPV6P71JlRbUY3TMNDtHUGaz0Kk9Vp38zOTgDQJAQ9GP
+ZPx863PijfkctcdC7lYc3iQxFmV0Hv8Rzdeq04ocwiPoOfM6tOq81zTiAK3tvQ6X
+qVds8Lg2GFumkt+LQQJBAPNRtm7w2D/THqXfZ0L/VBlITJfE285llCHZjwlHPjFL
+xlpm33izvdO66tW8atmMxLUyNzReChyRjoPEamIAflY=
+-----END RSA PRIVATE KEY-----`;
 
 const public_key_rsa = new NodeRSA(valor_públic_key);
 const private_key_rsa = new NodeRSA(valor_private_key);
+// La libreria del front para encryptar los datos usa el pkcs1 y tenemos que establecer que nosotros en node-rsa usaremos su mismo mecanismo para poder encryptar y descrifrar
+public_key_rsa.setOptions({encryptionScheme: 'pkcs1'});
+private_key_rsa.setOptions({encryptionScheme: 'pkcs1'});
 
 // encryptamiento por la llave publica
 const encryptMessage = public_key_rsa.encrypt(secret, 'base64');
@@ -45,13 +54,15 @@ console.log('mensaje encryptado: ', encryptMessage);
 const descriptionMessage = private_key_rsa.decrypt(encryptMessage, 'utf8');
 console.log('Secreto desencriptado: ', descriptionMessage);
 
-app.use(bodyParser.json());
-
 app.post('/Verificar', (req, res) => {
-    console.log(req.body);
+    console.log(req.body.data);
+    const keyvalue = req.body.data;
+    const descriptionMessage = private_key_rsa.decrypt(keyvalue, 'utf8');
+    console.log(descriptionMessage);
 
     res.status(200).json({
-        data: req.body
+        data: req.body,
+        descencrypted: descriptionMessage
     })
 });
 
